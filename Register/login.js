c767680f7461a16a5cd9af5e8613130cb57a2d50
SandyNabil <sandynabil257@gmail.com>
2025-01-29 21:57:21 +0200

    array of users done
    array of users in local storage
    and hide you don't have an acc if he has.
    and result icon.

--- Register/login.js
+++ Register/login.js
@@ -5,6 +5,9 @@ const passwordValidation = document.getElementById("passValidation");
 const emailValidation = document.getElementById("emailValidation");
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
+const haveAcoount = document.getElementById("haveAcc");
+
+haveAcoount.style.display = "none";
 console.log(savedMail);
 console.log(savedPass);
 
@@ -34,38 +37,39 @@ function validateAll() {
     emailValidation.innerText = "Email is required.";
     emailValidation.style.display = "block";
     return false;
-    }
-     if (!password) {
-       passwordValidation.innerText = "Password is required.";
-       passwordValidation.style.display = "block";
-       return false;
-    }
-    
-  if (email !== savedMail || password !== savedPass) {
-    // emailValidation.innerText = "This Email isn't registered !";
-    // emailValidation.style.display = "block";
-      // return false;
-
-      //security 
-      passwordValidation.innerText = "Incorrect Email or Password";
-      passwordValidation.style.display = "block";
-      return false;
   }
+  if (!password) {
+    passwordValidation.innerText = "Password is required.";
+    passwordValidation.style.display = "block";
+    return false;
+  }
+
+  //get the saved users 
+  let users = JSON.parse(localStorage.getItem("users")) || [];
+  //the specific logged user
+  let foundUser = users.find(  //return user object found
+    (user) => user.email === email && user.password === password
+  );
+  //if we didn't find the user 
+  if (!foundUser) {
+    passwordValidation.innerText = "Incorrect Email or Password";
+    passwordValidation.style.display = "block";
+    haveAcoount.style.display = "block";
+    return false;
+
+  }
+
+  // store the current user
+  localStorage.setItem("currentUser", JSON.stringify(foundUser));
   emailValidation.style.display = "none";
   return true;
 }
 
-// Form submission
+// we save the current user on login
 form.addEventListener("submit", function (e) {
-    e.preventDefault(); 
-    
-  const isValid = validateAll();
+  e.preventDefault();
 
-  if (
-    !isValid
-  ) {
-      return;  } else {
-    // Redirect to any page
+  if (validateAll()) {
     window.location.href = "../start exam/index.html";
   }
 });
